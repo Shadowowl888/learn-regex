7cfeb91103068cbd0f75ad31ea0eb1f4c0fb4a58
Shadowowl888 <lightningowl2004@gmail.com>
2022-06-13 21:26:31 -0700

    regex tutorial

---
+++ validation.js
@@ -0,0 +1,30 @@
+// var reg = /[a-z]/gi;
+// var reg2 = new RegExp(/[a-z]/, "gi");
+
+const inputs = document.querySelectorAll("input");
+
+// regex patterns
+const patterns = {
+  telephone: /^\d{11}$/,
+  username: /^[a-z\d]{5,12}$/i,
+  password: /^[\w@-]{8,20}$/i,
+  slug: /^[a-z\d-]{8,20}$/,
+  email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/,
+};
+
+// validation function
+function validate(field, regex) {
+  if (regex.test(field.value)) {
+    field.className = "valid";
+  } else {
+    field.className = "invalid";
+  }
+}
+
+// attach keyup event to inputs
+inputs.forEach((input) => {
+  input.addEventListener("keyup", (e) => {
+    // console.log(e.target.attributes.name.value);
+    validate(e.target, patterns[e.target.attributes.name.value]);
+  });
+});
